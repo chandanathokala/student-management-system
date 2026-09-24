e7e18acae4dbfa43651ef624739771c5244bd71e
chandanathokala <chandanathokala697@gmail.com>
2026-09-24 09:07:50 +0530

    Create apiException.js

---
+++ exception/apiException.js
@@ -0,0 +1,13 @@
+export class ApiException extends Error {
+
+    constructor(message, status) {
+
+        super(message);
+
+        this.name = "ApiException";
+
+        this.status = status;
+
+    }
+
+}
